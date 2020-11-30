785c6f8235e961a8963a325be887901609c55a51
Barret <991370140@qq.com>
2020-11-30 11:06:45 +0800

    kafka init

---
+++ kafka/src/main/java/com/example/kafka/config/KafkaInitialConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaInitialConfiguration {
+
+    @Bean
+    public NewTopic initialTopic() {
+        return new NewTopic("testtopic",8, (short) 2 );
+    }
+
+    // 如果要修改分区数，只需修改配置值重启项目即可
+    // 修改分区数并不会导致数据的丢失，但是分区数只能增大不能减小
+    @Bean
+    public NewTopic updateTopic() {
+        return new NewTopic("testtopic",10, (short) 2 );
+    }
+}
